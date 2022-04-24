f191894adca98a01c37e4da79a90e14a176b3fe3
demuko <tenkose@hotmail.com>
2022-04-24 23:59:18 +0900

    clean code

--- happy_lidar/happy_lidar/happy_lidar_node.py
+++ happy_lidar/happy_lidar/happy_lidar_node.py
@@ -18,12 +18,13 @@ from gazebo_msgs.srv import SpawnEntity, DeleteEntity
 
 class HappyLidar(Node):  # 簡単な移動クラス
     def __init__(self):   # コンストラクタ
-        super().__init__('happy_lidar_node')        
-        self.pub = self.create_publisher(Twist, 'cmd_vel', 10) 
-        self.sub = self.create_subscription(Odometry, 'odom', self.odom_cb, 10)    
-        self.sub = self.create_subscription(LaserScan, 'scan', self.lidar_cb, 10)   
+        super().__init__('happy_lidar_node')   
         self.timer = self.create_timer(0.01, self.timer_callback)
-        self.scan  = LaserScan()
+        self.pub = self.create_publisher(Twist, 'cmd_vel', 10) 
+        self.sub = self.create_subscription(Odometry, 'odom', self.odom_cb, 10)
+        # LiDARを使うために追加    
+        self.sub = self.create_subscription(LaserScan, 'scan', self.lidar_cb, 10)   
+        self.scan = LaserScan()       
  
         self.x, self.y, self.yaw = 0.0, 0.0, 0.0   
         self.x0, self.y0, self.yaw0 = 0.0, 0.0, 0.0
@@ -41,6 +42,46 @@ class HappyLidar(Node):  # 簡単な移動クラス
             (q_x, q_y, q_z, q_w))
         return x, y, yaw
 
+    def lidar_cb(self, msg):  # LiDARのコールバック関数
+        self.scan = msg
+
+        for i in range(len(msg.ranges)):
+            if msg.ranges[i] < msg.range_min or msg.ranges[i] > msg.range_max:
+                self.scan.ranges[i] = math.nan                
+            else:
+                self.scan.ranges[i] = msg.ranges[i] 
+    
+    def happy_lidar(self):
+        steps = 0
+        self.set_vel(0.0, 0.0)   
+        rclpy.spin_once(self)
+        self.load_gazebo_models()  # Doorのロード
+
+        while rclpy.ok():
+            print(f'step={steps}')            
+            
+            # Door open script
+            if len(self.scan.ranges) != 0:
+                if self.scan.ranges[0] > 0.5:
+                    #self.set_vel(0.2, 0.0)
+                    self.set_vel(0.0, 0.0)
+                else:
+                    self.set_vel(0.0, 0.0)
+
+            if steps == 100:
+                self.delete_gazebo_models()   # The door open 
+
+            rclpy.spin_once(self)
+            # self.print_lidar_info() # must be call after spin_once
+            if len(self.scan.ranges) != 0:
+                print(f'r[{  0}]={self.scan.ranges[0]} ')     # front
+                print(f'r[{ 90}]={self.scan.ranges[90]} ')    # left
+                print(f'r[{180}]={self.scan.ranges[180]} ')   # rear
+                print(f'r[{270}]={self.scan.ranges[270]} ')   # right
+  
+            time.sleep(0.1)  # 0.1 [s]
+            steps += 1
+    
     def print_lidar_info(self):
         # シミュレーションのPRDLIARは全周360[°] 計測可能。計測角度は-180[°]から180[°]。
         # ROSは右手系、進行方向x軸、左方向y軸、上方向がz軸（反時計まわりが正)。
@@ -62,32 +103,6 @@ class HappyLidar(Node):  # 簡単な移動クラス
         print(f'r[{270}]={self.scan.ranges[270]} ')
         print(f'r[{359}]={self.scan.ranges[359]} ')
 
-        #self.get_logger().info(
-        #    f'  0[deg]={self.scan.ranges[0]:.3f}[m]  90[deg]={self.scan.ranges[90]:.3f}[m]')
-        #self.get_logger().info(
-        #    f' 180[deg]={self.scan.ranges[180]:.3f}[m] -90[deg]={self.scan.ranges[270]:.3f}[m]')
-  
-    def lidar_cb(self, msg):         # オドメトリのコールバック関数
-        #self.scan = LaserScan()
-        self.scan = msg
-        #print(f'len(ranges)={len(msg.ranges)}')
-        #print(self.scan)
-
-        for i in range(len(msg.ranges)):
-            #print(f'r[{i}]={msg.ranges[i]} ')
-            #print(f'r[{i}]={self.scan.ranges[i]} ')
-            if msg.ranges[i] < msg.range_min or msg.ranges[i] > msg.range_max:
-                self.scan.ranges[i] = math.nan
-            else:
-                self.scan.ranges[i] = msg.ranges[i]
-        
-        #self.ranges          = msg.ranges
-        self.angle_min       = msg.angle_min  # スキャンの開始角度[rad]
-        self.angle_max       = msg.angle_max  # スキャンの終了角度[rad]
-        self.angle_increment = msg.angle_increment  # 角度分解能[rad]
-        self.range_min       = msg.range_min  # 最小検出距離[m] 
-        self.range_max       = msg.range_max  # 最大検出距離[m]
-      
     def odom_cb(self, msg):         # オドメトリのコールバック関数
         self.x, self.y, self.yaw = self.get_pose(msg)
         self.get_logger().info(
@@ -118,14 +133,8 @@ class HappyLidar(Node):  # 簡単な移動クラス
     #https://discourse.ros.org/t/spawning-a-robot-entity-using-a-node-with-gazebo-and-ros-2/9985
     #  Gazebo3Dモデルのロード
     def load_gazebo_models(self):
-        #load_node = rclpy.create_node('load_node')
         door_reference_frame='world'
         door_pose = Pose(position=Point(x=1.1, y=-0.2, z=0.5))
-
-        # 3Dモデルのパスを取得
-        #model_path = rospkg.RosPack().get_path('happy_mini_turtlebot3_sim')+'/turtlebot3_gazebo/models/'
-        #print(f'model_path={model_path}')
-        model_path = '/home/ubuntu/airobot_ws/src/chapter2/happy_mini_turtlebot3_sim/turtlebot3_gazebo/models/'
 
         client = self.create_client(SpawnEntity, "/spawn_entity")
 
@@ -134,10 +143,8 @@ class HappyLidar(Node):  # 簡単な移動クラス
             client.wait_for_service()
             self.get_logger().info("connected!")
         
-        # Get path to the door
-        sdf_file_path = os.path.join(
-            get_package_share_directory("turtlebot3_gazebo"), "models",
-            "door", "model.sdf")
+        home_dir = os.environ['HOME']    
+        sdf_file_path = home_dir + '/airobot_ws/src/chapter4/happy_lidar/models/door/model.sdf'
 
         # Set data for request
         request = SpawnEntity.Request()
@@ -145,12 +152,12 @@ class HappyLidar(Node):  # 簡単な移動クラス
         request.xml = open(sdf_file_path, 'r').read()
         request.robot_namespace = ''
         request.reference_frame = 'world'
-        request.initial_pose.position.x = 1.1
+        request.initial_pose.position.x =  1.1
         request.initial_pose.position.y = -0.2
-        request.initial_pose.position.z = 0.5
+        request.initial_pose.position.z =  0.5
 
         self.get_logger().info("Sending service request to `/spawn_entity`")
-        future =  client.call_async(request)
+        future = client.call_async(request)
         rclpy.spin_until_future_complete(self, future)
         if future.result() is not None:
             print('response: %r' % future.result())
@@ -158,12 +165,10 @@ class HappyLidar(Node):  # 簡単な移動クラス
             raise RuntimeError(
                 'exception while calling service: %r' % future.exception())
 
-        #load_node.destroy_node()
 
     #  Gazebo 3Dモデルの削除
     def delete_gazebo_models(self):
         print('delete_gazebo_models')
-        #delete_node = rclpy.create_node('delete_node')
         client = self.create_client(DeleteEntity, '/delete_entity')
         while not client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not availabe. waiting...')
@@ -179,35 +184,7 @@ class HappyLidar(Node):  # 簡単な移動クラス
         else:
             raise RuntimeError(
                 'exception while calling service: %r' % future.exception())
-        
-        #delete_node.destroy_node()
-
-
-    def happy_lidar(self): 
-        duration = 0.1
-        steps = 0
-
-        #  Doorのロード
-        print('load_gazebo_models')
-        self.load_gazebo_models()
-
-
-        while rclpy.ok():
-            # ハンズオン１：ここに、何かに0.3[m]以内に近づいたら停止するコードを書く
-            print(f'step={steps}')            
-
-            print(f'r[{0}]={self.scan.ranges[0]} ')
-            if self.scan.ranges[0] > 1.0:
-                self.set_vel(0.2,0.0)
-
-            # print(f'duration*steps={duration*steps}')
-            if duration * steps == 10:
-                print('call delete_gazebo_models ')
-                self.delete_gazebo_models()    
-            rclpy.spin_once(self)
-            # self.print_lidar_info() # after spin_once
-            time.sleep(duration)
-            steps += 1
+  
 
 def main(args=None):  # main関数
     rclpy.init(args=args)
